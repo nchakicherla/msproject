7f9a2c6465da23f3849c1908bca4ce2931e54905
naveenchaki <nchaki6@gmail.com>
2020-03-31 18:05:43 -0700

    Added camera and lighting change trigger methods

--- syncBelt.py
+++ syncBelt.py
@@ -3,7 +3,7 @@ class syncBelt:
     def __init__(self, interval):
         self.interval = interval
         
-    def motor(self, duration):
+    def motor(self):
         
         # import RPi.GPIO module for using of pins
         import RPi.GPIO as GPIO
@@ -25,11 +25,9 @@ class syncBelt:
                     print("Input pin 8 is LOW - Output pin 2 is LOW")
                     GPIO.output(3, 0)           # set pin 3 output to LOW/0
                 sleep(interval)                 # wait for specified amount of time before repolling (s)
-                duration = duration - interval  # iterate duration downwards until it equals 0 (motor operation complete)
-                print(duration + " seconds remain")
 
         except:
-            print("An exception occurred")  # if code is unsuccessful return error message
+            print("An exception occurred (motor)")  # if code is unsuccessful return error message
             
         finally:
             GPIO.cleanup()                  # revert settings to prevent damage to GPIO and RPi
@@ -38,10 +36,47 @@ class syncBelt:
 
         # import RPi.GPIO module for using pins
         import RPi.GPIO as GPIO
-        from time import sleep
-
         
+        GPIO.setmode(GPIO.BOARD)
 
-        
+        GPIO.setup(7, GPIO.OUT)
+        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # input pin for button input, different button with but connected to same 3v3 power pin 1
 
+        try:
+            if GPIO.input(12):
+                print("Input pin 12 is HIGH - trigger camera")
+                GPIO.output(7, 1)           # set pin 7 output to HIGH/1, trigger camera
+            else:
+                print("Input pin 12 is LOW - don't trigger camera")
+                GPIO.output(7, 0)           # set pin 7 output to LOW/0
+
+        except:
+            print("An exception occurred (camera)"
+
+        finally:
+            GPIO.cleanup()
+
+    def light(self):
+
+        import RPi.GPIO as GPIO
+
+        GPIO.setmode(GPIO.BOARD)
+
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # using same input pin as camera button, camera will trigger once every light change (6 times)
+
+        try:
+            if GPIO.input(12):
+                print("Input pin 12 is HIGH - trigger lighting change")
+                GPIO.output(11, 1)          # set pin 11 output to HIGH/1, trigger light change
+            else:
+                print("Input pin 12 is LOW - don't trigger lighting change")
+                GPIO.output(11, 0)          # set pin 11 output to LOW/0, do not trigger light change
+
+        except:
+            print("An exception occurred (light)")
+
+        finally:
+            GPIO.cleanup()
+            
     
