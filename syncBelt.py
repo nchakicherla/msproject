a59418be3a853bc39fbda44a58d9d0f357b0056e
naveenchaki <nchaki6@gmail.com>
2020-03-31 21:52:43 -0700

    Reorganized motor() method

--- syncBelt.py
+++ syncBelt.py
@@ -6,7 +6,7 @@ class syncBelt:
         
     def motor(self):
         
-        # import RPi.GPIO module for using of pins
+        # import RPi.GPIO module for using pins
         import RPi.GPIO as GPIO
         from time import sleep
 
@@ -14,20 +14,19 @@ class syncBelt:
         GPIO.setmode(GPIO.BOARD)
 
         # designate each pin as input/output and initial values
-        GPIO.setup(3, GPIO.OUT)                             # output pin for control of motor via TIP120 base lead (GPIO2)
-        GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # input pin for button input, pulled down to provide discrete signal (GPIO14)
+        GPIO.setup(3, GPIO.OUT)                 # output pin for control of motor via TIP120 base lead (GPIO2)
+        # MOVE TO SYNCHRONIZE: GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # input pin for button input, pulled down to provide discrete signal (GPIO14)
         
         try:
-            while duration != 0:
-                if GPIO.input(8):
-                    print("Input pin 8 is HIGH - Output pin 2 is HIGH")
-                    GPIO.output(3, 1)           # set pin 3 output to HIGH/1
-                else:
-                    print("Input pin 8 is LOW - Output pin 2 is LOW")
-                    GPIO.output(3, 0)           # set pin 3 output to LOW/0
+            while True:
+                print("Motor active")
+                GPIO.output(3, 1)
                 sleep(interval)                 # wait for specified amount of time before repolling (s)
                 duration = duration - interval  # iterate duration downwards by interval (s) until duration is 0
+                if duration == 0:               
+                    break
                 
+            print("Cycle finished")
 
         except:
             print("An exception occurred (motor)")  # if code is unsuccessful return error message
