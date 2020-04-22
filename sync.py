174a6a15bc3221abbb52b4c661ad38ca3a1415bb
System Administrator <root@Naveens-MacBook-Air.local>
2020-04-22 02:47:14 -0700

    General edits

--- sync.py
+++ sync.py
@@ -26,7 +26,7 @@ print("Pins have been setup, waiting for signal to start motor operation")
 def motor(timeon, timeoff, cycles, profile):
     n = cycles
     if profile == 0:
-        while cycles != 1:
+        while cycles > 0:
             a = (n - cycles) + 1
             GPIO.add_event_detect(15, GPIO.RISING)
             if GPIO.event_detected(15):
@@ -37,10 +37,10 @@ def motor(timeon, timeoff, cycles, profile):
             GPIO.output(7, 0)
             sleep(timeoff/2)
             cam = PiCamera()
-            camera.start_preview()
-            camera.capture('/home/pi/Pictures/%s.jpg', a)
+            #cam.start_preview()
+            cam.capture('/home/pi/Pictures/%s.jpg', a)
             sleep(timeoff/2)
-            camera.stop_preview()
+            #cam.stop_preview()
             cycles = cycles - 1
             if cycles == 0:
                 break
@@ -52,24 +52,28 @@ def motor(timeon, timeoff, cycles, profile):
             GPIO.add_event_detect(15, GPIO.RISING)
             # profile for
 
-
 try:
-    with open('config.txt','r') as conf:
+
+    with open('config.txt', 'r') as conf:
         x = conf.read().splitlines()
     on = x[0]; off = x[1]; cyc = x[2]; prof = x[3]
 
-    GPIO.add_event_detect(15, GPIO.RISING)
-    m = threading.Thread(target=motor, args=(on,off,cyc,prof))
-    if GPIO.event_detected(15):
-        m.start()
-        print("Button press detected. Beginning motor cycling.")
-        m.join()
-        print("Cycling complete.")
-    else:
-        print("Waiting for button press.")
-        sleep(0.1)
+    while True:
+        GPIO.add_event_detect(15, GPIO.RISING)
+        m = threading.Thread(target=motor, args=(on,off,cyc,prof))
+        if GPIO.event_detected(15):
+            m.start()
+            print("Button press detected. Beginning motor cycling.")
+            m.join()
+            print("Cycling complete.")
+
 except KeyboardInterrupt:
-    print("Interrupt received.")
+    return "Interrupt received"
+
+finally:
+    GPIO.cleanup()
+
+
 
 
 
