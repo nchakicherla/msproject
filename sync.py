ca3502c331c5746a60c82298d6efdec36b3bb314
naveenchaki <nchaki6@gmail.com>
2020-04-28 05:24:49 -0700

    Added removal of button press detection during execution of motor cycle function

--- sync.py
+++ sync.py
@@ -8,29 +8,24 @@ from time import sleep
 import board
 import neopixel
 
-
 GPIO.setwarnings(False)
-
-# button input = 15, motor output = 7
-GPIO.setmode(GPIO.BOARD)
-
+GPIO.setmode(GPIO.BOARD) # button input = 15, motor output = 7
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(7, GPIO.OUT)
-
 GPIO.output(7, 0)
 
-pixels = neopixel,NeoPixel(board.D18, x) # x = number of LEDs
-
-print("Pins have been setup, waiting for signal to start motor operation")
+pixels = neopixel.NeoPixel(board.D18, x) # x = number of LEDs
 
 def motor(timeon, timeoff, cycles, profile):
     n = cycles
-    if profile == 0:
+    if profile == 1:
         while cycles > 0:
             a = (n - cycles) + 1
-            GPIO.add_event_detect(15, GPIO.RISING)
-            if GPIO.event_detected(15):
-                break
+            #GPIO.add_event_detect(15, GPIO.RISING)
+            #if GPIO.event_detected(15):
+            #    break
+            #else:
+            #    continue
             GPIO.output(7, 1)
             print("Motor in operation.")
             sleep(timeon)
@@ -44,38 +39,44 @@ def motor(timeon, timeoff, cycles, profile):
             cycles = cycles - 1
             if cycles == 0:
                 break
+            else:
+                continue
         print("Motor cycling complete.")
 
-    if profile == 1:
+    if profile == 2:
         while cycles !=1:
             b = (n - cycles) + 1
-            GPIO.add_event_detect(15, GPIO.RISING)
+            #GPIO.add_event_detect(15, GPIO.RISING)
             # profile for
+
+print("Pins have been setup, waiting for signal to start motor operation")
 
 try:
 
     with open('config.txt', 'r') as conf:
         x = conf.read().splitlines()
     on = x[0]; off = x[1]; cyc = x[2]; prof = x[3]
+    m = threading.Thread(target=motor, args=(on, off, cyc, prof))
+    GPIO.add_event_detect(15, GPIO.RISING)
 
     while True:
-        GPIO.add_event_detect(15, GPIO.RISING)
-        m = threading.Thread(target=motor, args=(on,off,cyc,prof))
+        while GPIO.input(15) == 0:
+            sleep(0.01)
         if GPIO.event_detected(15):
+            GPIO.remove_event_detect(15)
             m.start()
             print("Button press detected. Beginning motor cycling.")
             m.join()
             print("Cycling complete.")
+            continue
+        else:
+            continue
 
 except KeyboardInterrupt:
-    return "Interrupt received"
+    print("Interrupt received. Exiting.")
 
 finally:
     GPIO.cleanup()
-
-
-
-
 
 
 
