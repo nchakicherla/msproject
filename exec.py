b64f954b2a493e590c091fd853781b0a585e60e8
naveenchaki <naveenchaki@gmail.com>
2020-07-23 18:20:00 -0700

    Added calibration procedure to calculate time for full rotation

--- exec.py
+++ exec.py
@@ -4,13 +4,13 @@
 import RPi.GPIO as GPIO
 import threading
 from picamera import PiCamera
-from time import sleep
+import time
 import board
 import neopixel
 
-detectpin = 22
-proceedpin = 27
-motorpin = 3
+detectpin = 22 # pin used for detecting button presses
+motorpin = 3 # pin used with TIP120 for controlling motor
+numLED = 58 # number of LEDs to be used with NeoPixel
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM) # button input = 15, motor output = 7
@@ -22,6 +22,25 @@ GPIO.output(motorpin, 0)
 
 def motor():
 
+# Method for determining how long it takes for full rotation of belt
+
+def rotationtime():
+    while True:
+        if GPIO.event_detected(detectpin):
+            GPIO.output(motorpin, 0)
+            break
+    return
+
+def calibrate():
+    GPIO.add_event_detect(detectpin, GPIO.RISING, bouncetime=500)
+    if GPIO.event_detected(detectpin):
+        GPIO.output(motorpin, 1)
+        begin = time.perf_counter()
+        rotationtime()
+        end = time.perf_counter()
+        looptime = end - begin # Calculate time for full rotation for later use
+
+
 # Retrieve operation parameters from user
 
 g = input("Enter the number of strip groups (1-4):")
@@ -29,6 +48,11 @@ n = input("Enter the number of strips per group (1-4):")
 i = input("Enter lighting profile to be used:")
 t = input("Enter time to wait between images (seconds):")
 f = input("Should images be obtained at t = 0? (y/n):")
+
+# Start with loading cycle, where reactions are initiated and strips are loaded onto belt
+
+lights = neopixel.NeoPixel(board.D18, numLED)
+lights.fill((255, 255, 255)) # light up all LEDs white to ease loading process
 
 
 
