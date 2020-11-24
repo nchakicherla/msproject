bfa9aa1acb4f0f53c82aa59cf54710f711c81b28
naveenchaki <nchaki6@gmail.com>
2020-11-24 11:43:44 -0800

    Added license information, correction method

--- exec.py
+++ exec.py
@@ -1,7 +1,157 @@
 # Python script used to control motor/belt, LEDs for lighting, and camera for image acquisition. Each component
 # is controlled by a self-contained function that is called when needed
 
+"""
+RPI.GPIO Library
+
+Copyright (c) 2012-2014 Ben Croston
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+PiCamera Library
+
+Copyright 2013-2015 Dave Jones
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+    Neither the name of the copyright holder nor the names of its contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+Adafruit CircuitPython NeoPixel Library
+
+Copyright (c) 2013, 2014 Damien P. George
+Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+NumPy Library
+
+
+Copyright (c) 2005-2020, NumPy Developers.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+       copyright notice, this list of conditions and the following
+       disclaimer in the documentation and/or other materials provided
+       with the distribution.
+
+    * Neither the name of the NumPy Developers nor the names of any
+       contributors may be used to endorse or promote products derived
+       from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+OpenCV (cv2)
+
+Copyright (C) 2000-2020, Intel Corporation, all rights reserved.
+Copyright (C) 2009-2011, Willow Garage Inc., all rights reserved.
+Copyright (C) 2009-2016, NVIDIA Corporation, all rights reserved.
+Copyright (C) 2010-2013, Advanced Micro Devices, Inc., all rights reserved.
+Copyright (C) 2015-2016, OpenCV Foundation, all rights reserved.
+Copyright (C) 2015-2016, Itseez Inc., all rights reserved.
+Copyright (C) 2019-2020, Xperience AI, all rights reserved.
+Third party copyrights are property of their respective owners.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are disclaimed.
+In no event shall copyright holders or contributors be liable for any direct,
+indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+"""
+
+
 from typing import TextIO
+import os
 import sys
 import threading
 import RPi.GPIO as GPIO # pylint: disable=import-error
@@ -9,6 +159,8 @@ import picamera # pylint: disable=import-error
 import time # pylint: disable=import-error
 import board # pylint: disable=import-error
 import neopixel # pylint: disable=import-error
+import numpy as np
+import cv2
 
 detectpin = 22  # pin used for detecting button presses
 motorpin = 3  # pin used with TIP120 for controlling motor
@@ -26,9 +178,10 @@ cam = picamera.PiCamera()
 cam.resolution = (3280, 2464)
 cam.start_preview(fullscreen=False, window=(800, 400, 640, 480))  # draw camera preview
 
-lights = neopixel.NeoPixel(board.D18, numLED)  # create NeoPixel object for lights
-r = [[255, 197, 143], [255, 214, 170], [255, 241, 224],
-     [255, 250, 244], [255, 255, 255]]  # create 2D array of RGB values for common light colors
+def enableLights():
+    lights = neopixel.NeoPixel(board.D18, numLED)  # create NeoPixel object for lights
+    r = [[255, 197, 143], [255, 214, 170], [255, 241, 224],
+         [255, 250, 244], [255, 255, 255]]  # create 2D array of RGB values for common light colors
 
 
 # Method for activating motor/belt
@@ -72,7 +225,7 @@ def loading(clusters, initial):
     global cyclecount
     cyclecount = 1
     for i in range(clusters):
-        for j in range(stripsCluster[i]):
+        for j in range(stripsPerCluster[i]):
             GPIO.add_event_detect(detectpin, GPIO.RISING, bouncetime=buttonbounce)
             print("Load strip, and press button when strip is securely placed on belt.")
             # User initiates chemistry, loads strip, then presses button to continue.
@@ -85,6 +238,7 @@ def loading(clusters, initial):
                     lights.fill((255, 255, 255))
                     time.sleep(1)
                     cam.capture('{0}_{1}_{2}.jpg'.format(i, j, cyclecount))
+                    # files are saved with format cluster_stripincluster_cyclecount
                 elif lightingCluster[i] == 2:
                     for a in range(4):
                         lights.fill((r[a][0], r[a][1], r[a][2]))
@@ -105,8 +259,8 @@ def loading(clusters, initial):
 
             GPIO.remove_event_detect(detectpin)
             print("Motor in motion. Please prepare next strip in cluster")
-            motor(rot_time / stripsCluster[i])
-        motor(rot_time / (stripsCluster[i] * clusters))
+            motor(rot_time / stripsPerCluster[i])
+        motor(rot_time / (stripsPerCluster[i] * clusters))
     cyclecount += 1
 
 
@@ -114,7 +268,7 @@ def loading(clusters, initial):
 def cycling():
     global cyclecount
     for i in range(g):
-        for j in range(stripsCluster[i]):
+        for j in range(stripsPerCluster[i]):
             if lightingCluster[i] == 1:
                 lights.fill((255, 255, 255))
                 time.sleep(1)
@@ -130,45 +284,102 @@ def cycling():
                     lights.fill((r[a][0], r[a][1], r[a][2]))
                     time.sleep(1)
                     cam.capture('{0}_{1}_{2}_{3}.jpg'.format(i, j, cyclecount, a))
-            motor(rot_time / stripsCluster[i])
-        motor(rot_time / (stripsCluster[i] * g))
+            motor(rot_time / stripsPerCluster[i])
+        motor(rot_time / (stripsPerCluster[i] * g))
     cyclecount += 1
+
+def getCorrection():
+    checkX = [142, 171, 199, 242, 284, 324, 370, 413, 461, 511, 558, 604, 652, 705, 752, 804, 856, 907, 960, 1012]
+    checkY = [520, 497, 476, 440, 413, 388, 365, 345, 330, 317, 309, 298, 293, 290, 284, 286, 295, 298, 311, 323]
+    boundaries = ([150,30,30],[255,255,255])
+    cam.resolution = (1280, 720)
+
+    cam.capture('correction.jpg')
+    image = cv2.imread("correction.jpg")
+
+    lower = np.array(boundaries[0], dtype = "uint8")
+    upper = np.array(boundaries[1], dtype = "uint8")
+
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask = mask)
+
+    # lists of x and y coordinates that tell program where to look for blue sticker
+    checkX = [142, 171, 199, 242, 284, 324, 370, 413, 461, 511, 558, 604, 652, 705, 752, 804, 856, 907, 960, 1012]
+    checkY = [520, 497, 476, 440, 413, 388, 365, 345, 330, 317, 309, 298, 293, 290, 284, 286, 295, 298, 311, 323]
+    # x = 717, y = 293
+    # output[y][x]
+
+    #check all predefined locations for sticker to determine where strip is
+    for spotCheck in range(20):
+        if np.any(output[checkY[spotCheck]][checkX[spotCheck]] != False):
+            print("Belt in position " + str(spotCheck))
+            break
+
+    #cv2.imwrite("correctionOut.jpg", output)
+
+
+
+
 
 
 # Program starts here unless imported
 if __name__ == "__main__":
     # Retrieve operation parameters from user
-    print("This program was created to automate the photography of serum creatinine test strips.\n\n")
+    print("This program was created to automate the photography of diagnostic test strips.\n\n")
 
     while True:
         g = int(input("Enter the number of strip clusters (1-2)..."))
         if 1 <= g <= 2:
             break
 
-    stripsCluster = []
+    stripsPerCluster = []
     while True:
         for cluster in range(g):
-            n = int(input("Enter the number of strips in cluster " + str(cluster) + "\n"))
-            stripsCluster.append(n)
-        if len(stripsCluster) == g:
-            break
-        else:
-            print("Invalid entries for number of strips in each cluster. Please try again.\n\n")
+            while True:
+                n = int(input("Enter the number of strips in cluster " + str(cluster) + "\n"))
+                if 1 <= n <= 4:
+                    stripsPerCluster.append(n)
+                    break
+
+        while True:
+            if len(stripsPerCluster) == g:
+                print(stripsPerCluster)
+                print("\n")
+                confirmation = input("Is this the correct number of strips in each cluster?")
+                if confirmation == "y":
+                    break
+
+            else:
+                print("Invalid entries for number of strips in each cluster. Please try again.\n\n")
+                continue
 
     lightingCluster = []
     while True:
         for cluster in range(g):
-            p = int(input("Enter the lighting profile to be used for cluster" + str(cluster) + "\n"))
-            lightingCluster.append(p)
-        # p = int(input("Enter lighting profile to be used (1-3)..."))  # 1 = 4 colors, 2 = white light
-        if len(lightingCluster) == g:
-            break
+            while True:
+                p = int(input("Enter the lighting profile to be used for cluster" + str(cluster) + "\n"))
+                if 1 <= p <= 2:
+                    lightingCluster.append(p)
+                    break
+
+        # p = int(input("Enter lighting profile to be used (1-3)..."))  # 1 = white light, 2 = 4 colors
+        # support for multiple colors coming later, profiles should all be 1
+        while True:
+            if len(lightingCluster) == g:
+                print(lightingCluster)
+                print("\n")
+                confirmation = input("Is this the correct lighting profile for each cluster?")
+                if confirmation == "y":
+                    break
+                else
+                    continue
         else:
             print("Invalid entries for lighting profile to be used for each cluster. Please try again.\n\n")
+            continue
 
     while True:
         t = int(input("Enter time to wait between images (seconds)..."))
-        if t > 0:
+        if t > 80:
             break
     while True:
         f = str(input("Should images be obtained at t = 0? (y/n)..."))
@@ -232,5 +443,6 @@ if __name__ == "__main__":
         tracker = t - (endcycle - begincycle)
         print("Waiting for next photo cycle.")
 
-    print("Process complete. All cycles have been completed.")
+    print("Process complete. All cycles have been completed.\n")
+    print("Images are saved in current directory")
     sys.exit(0)
